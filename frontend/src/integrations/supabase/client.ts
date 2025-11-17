626da1b2b65d2677dc309a82d66eb0946360d478
Edward Mensah <edward.mensah@ashesi.edu.gh>
2025-11-17 11:05:38 +0000

    removed the consoling of the env variables

--- frontend/src/integrations/supabase/client.ts
+++ frontend/src/integrations/supabase/client.ts
@@ -5,5 +5,5 @@ import type { Database } from './types';
 
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
-console.log(SUPABASE_URL, SUPABASE_ANON_KEY);
+
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
